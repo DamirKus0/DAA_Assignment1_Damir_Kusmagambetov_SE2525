824d4a19b450eb9414f7c84917073554f8fe1149
DamirKus0 <justkusta@gmail.com>
2026-09-19 18:36:30 +0500

    Added Metrics class for comparisons and recursion depth

---
+++ src/main/java/Task/Metrics.java
@@ -0,0 +1,24 @@
+package Task;
+public class Metrics {
+    private long comparisons = 0;
+    private int maxDepth = 0;
+    private int currentDepth = 0;
+
+    public void incrementComparisons() {
+        comparisons++;
+    }
+
+    public void enterRecursion() {
+        currentDepth++;
+        if (currentDepth > maxDepth) {
+            maxDepth = currentDepth;
+        }
+    }
+
+    public void exitRecursion() {
+        currentDepth--;
+    }
+
+    public long getComparisons() { return comparisons; }
+    public int getMaxDepth() { return maxDepth; }
+}

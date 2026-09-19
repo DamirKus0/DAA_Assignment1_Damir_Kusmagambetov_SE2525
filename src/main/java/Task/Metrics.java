25ffe619156abcedbdf54320ecbf9ed72d2398d6
DamirKus0 <justkusta@gmail.com>
2026-09-19 18:38:35 +0500

    Added Metrics class for comparisons and recursion depth

--- src/main/java/Task/Metrics.java
+++ src/main/java/Task/Metrics.java
@@ -1,24 +1,54 @@
 package Task;
+
 public class Metrics {
-    private long comparisons = 0;
-    private int maxDepth = 0;
-    private int currentDepth = 0;
+    private long comparisons;
+    private int maxDepth;
+    private int currentDepth;
+    private long startTime;
+    private long endTime;
+
+    public Metrics() {
+        this.comparisons = 0;
+        this.maxDepth = 0;
+        this.currentDepth = 0;
+    }
+
+    public void startTimer() {
+        this.startTime = System.nanoTime();
+    }
+
+    public void stopTimer() {
+        this.endTime = System.nanoTime();
+    }
+
+    public long getTimeInMilliseconds() {
+        return (endTime - startTime) / 1_000_000;
+    }
+
+    public long getTimeInNanoseconds() {
+        return endTime - startTime;
+    }
 
     public void incrementComparisons() {
         comparisons++;
     }
 
-    public void enterRecursion() {
+    public void incrementDepth() {
         currentDepth++;
         if (currentDepth > maxDepth) {
             maxDepth = currentDepth;
         }
     }
 
-    public void exitRecursion() {
+    public void decrementDepth() {
         currentDepth--;
     }
 
-    public long getComparisons() { return comparisons; }
-    public int getMaxDepth() { return maxDepth; }
+    public long getComparisons() {
+        return comparisons;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
 }
